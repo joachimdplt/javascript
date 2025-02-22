476a28b00614f90b97adc307e0904938c4f0b09c
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-02-22 15:18:38 +0100

    Add click count to btnBlue

--- initiation/script.js
+++ initiation/script.js
@@ -4,6 +4,8 @@ title.style.color ="green";
 console.log(title);
 // Selected & stock
 const btnRed = document.getElementById("btn-red");
+const btnBlue = document.getElementById("btn-blue");
+
 let count = 0;
 console.log(count, "count start")
 
@@ -13,3 +15,9 @@ btnRed.addEventListener('click', function(){
     console.log(count, "incremented count")
     //console.log('cliqued');
 })
+btnBlue.addEventListener('click', function(){
+    count = count + 1;
+    console.log(count, "incremented count")
+    //console.log('cliqued');
+})
+
