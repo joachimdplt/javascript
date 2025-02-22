07b323053bab94e235b9d8c715d2b9dd2f5ef9ff
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-02-22 19:00:32 +0100

    Select and stock variables

---
+++ cookies/script.js
@@ -0,0 +1,5 @@
+// Select and stock variables
+const btnSuccess = document.querySelector('.btn-success')
+console.log(btnSuccess);
+const cookies = document.querySelector('.cookies')
+console.log(cookies);
