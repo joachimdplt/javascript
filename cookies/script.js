7e7ae9232fb043410aeb7af5971c915d81e95af0
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-02-22 19:04:13 +0100

    Add event listener

--- cookies/script.js
+++ cookies/script.js
@@ -3,3 +3,10 @@ const btnSuccess = document.querySelector('.btn-success')
 console.log(btnSuccess);
 const cookies = document.querySelector('.cookies')
 console.log(cookies);
+
+// Add event Listener
+btnSuccess.addEventListener('click', function() {
+    console.log('bouton cliqué');
+    cookies.style.opacity = "0";
+});
+
