2d3781366123d6d95b24f3c1b19b22e1c333cced
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-02-22 13:02:46 +0100

    Creation little game, select & stock red button

--- initiation/script.js
+++ initiation/script.js
@@ -1,2 +1,9 @@
-console.log('connected')
-document.getElementById('title').style.color ="green";
+console.log('connected');
+const title = document.getElementById('title');
+title.style.color ="green";
+console.log(title);
+// Selected & stock
+const btnRed = document.getElementById("btn-red");
+console.log(btnRed, "button red");
+
+
